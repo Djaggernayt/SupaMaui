11c115f13fb32701b8267f6d9cfcb61536afd35a
Djaggernayt <91201251+Djaggernayt@users.noreply.github.com>
2026-01-23 21:09:12 +0300

    sgfdsf

--- SupaMaui/MainPage.xaml.cs
+++ SupaMaui/MainPage.xaml.cs
@@ -1,6 +1,10 @@
 ﻿
 
+using Microsoft.Maui.Controls.Platform;
 using Supabase;
+using static Supabase.Gotrue.Constants;
+
+
 
 namespace SupaMaui
 {
@@ -8,13 +12,20 @@ namespace SupaMaui
     {
         private readonly Client _supabase;
 
+
+
         public MainPage(Client supabase)
         {
             InitializeComponent();
             _supabase = supabase;
             LoadProduct();
+           // AddUser();
 
+        }
 
+        async void AddUser()
+        {
+            var session = await _supabase.Auth.SignUp("test@localhost.com","admin1234");
         }
 
         async void LoadProduct()
@@ -32,14 +43,26 @@ namespace SupaMaui
         }
         
 
-        private void add_Clicked(object sender, EventArgs e)
+        private async void add_Clicked(object sender, EventArgs e)
         {
-
+            await _supabase.Auth.VerifyOTP(email.Text, otp.Text, EmailOtpType.Email);
         }
 
         private void del_Clicked(object sender, EventArgs e)
         {
 
+        }
+
+        private async void enter_Clicked(object sender, EventArgs e)
+        {
+
+                await _supabase.Auth.SignIn(email.Text, password.Text);
+            // где то тут должна быть проверка
+                var option = new Supabase.Gotrue.SignInWithPasswordlessEmailOptions(email.Text);
+                await _supabase.Auth.SignInWithOtp(option);
+
+
+            
         }
     }
 
